2906b4604b0a3a10f7dab5bb6aa151b29867f21c
aym0125 <raintree0125@gmail.com>
2017-12-31 18:34:28 +0900

    reset

--- public/javascript/index.js
+++
@@ -1,9 +0,0 @@
-var image_onclick = function (event) {
-    window.alert('Hello World !');
-};
- 
-var document_onready = function (event) {
-    $('#img').on('click', image_onclick);
-};
- 
-$(document).ready(document_onready);
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
- 
-// デフォルトルーティング
-router.get('/', function (request, response) {
-    response.render('index', { title: 'Sample Node.js', message: 'Hello there!' });
-});
- 
-module.exports = router;
